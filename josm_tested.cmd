42d641dffab111bb4ffbb745a0e18da499e5196d
Jo <winfixit@gmail.com>
2017-04-20 15:27:08 +0200

    added some comments to CMD file

--- josm_tested.cmd
+++ josm_tested.cmd
@@ -1,4 +1,7 @@
+REM /D will make sure an existing configuration doesn't get replaced
+
 xcopy /D preferences.xml %USERPROFILE%\AppData\Roaming\JOSM\
 
+REM this CMD file is mostly needed to make sure the following Powershell script can be executed, regardless of execution policy
 
 powershell -noexit -executionpolicy bypass -File .\Download_OSM.ps1 -josm tested
