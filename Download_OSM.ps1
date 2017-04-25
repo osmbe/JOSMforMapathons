235e2233d54334d02c42bdb1d7e732b5f37f98f4
Jo <winfixit@gmail.com>
2017-04-25 08:27:28 +0200

    added code to download plugins too

--- Download_OSM.ps1
+++ Download_OSM.ps1
@@ -4,15 +4,23 @@ param(
   [ValidateSet("tested", "latest")]
   [string]$josm = "tested"
 )
-
+$start_time = Get-Date
 $url = "https://josm.openstreetmap.de/download/josm-{0}.jar" -f $josm
 $output = [Environment]::GetFolderPath("Desktop") + "\josm-{0}.jar" -f $josm
-$start_time = Get-Date
-
-echo $output
+$pluginfolder = [Environment]::GetFolderPath("UserProfile") + "\AppData\Roaming\JOSM\plugins"
+$pluginsurl = "https://svn.openstreetmap.org/applications/editors/josm/dist/"
 
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
+
+ForEach ($plugin In "OpeningHoursEditor","RoadSigns","apache-commons","apache-http","buildings_tools","geochat","measurement","pt_assistant","tag2link","terracer","tofix","utilsplugin2","wikipedia" )
+ {echo "$pluginsurl$plugin"
+ Start-BitsTransfer -Source "$pluginsurl$plugin.jar" -Destination "$pluginfolder\$plugin"}
+
+Start-BitsTransfer -Source "https://github.com/JOSM/Mapillary/releases/download/v1.5.3/Mapillary.jar" -Destination "$pluginfolder\$plugin"
+Start-BitsTransfer -Source "http://developer.skobbler.com/public/openstreetcam/OpenStreetCam.jar" -Destination "$pluginfolder\$plugin"
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/Gubaer/josm-scripting-plugin/deploy/dist/scripting.jar" -Destination "$pluginfolder\$plugin"
+Start-BitsTransfer -Source "https://github.com/JOSM/todo/releases/download/v30100/todo.jar" -Destination "$pluginfolder\$plugin"
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
